7ade8b39f5032aa344f1d45dd887601b4b026782
Cleyton Melo <cleytonw11@gmail.com>
2022-11-04 22:16:14 -0300

    adding JavaScript

---
+++ main.js
@@ -0,0 +1,37 @@
+function createGame(player1, hour, player2) {
+ return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="icone da bandeira do ${player1}">
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="icone da bandeira da ${player2}">
+    </li>
+ `
+}
+
+function createCard(date, day, games) {
+  return `
+  <div class="card">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+      ${games}
+    </ul>
+  </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="logo do calendário nlw da copa">
+  </header>
+  <main id="cards">
+    ${createCard("24/11", "quinta", 
+      createGame("brazil", "16:00", "serbia")
+    )}
+    ${createCard("28/11","segunda",
+      createGame("brazil", "13:00", "switzerland")
+    )}
+    ${createCard("02/12", "sexta", 
+      createGame("cameroon", "16:00", "brazil")
+    )}
+  </main>
+`
